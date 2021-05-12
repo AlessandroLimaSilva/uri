54339d7d717a944c4398f47ce82859fb5bab0fac
AlessandroLimaSilva <coderlima@gmail.com>
2021-05-12 00:54:23 -0300

    Exercicio URI1095

---
+++ src/desafio/desafio/URI1095.java
@@ -0,0 +1,26 @@
+package desafio;
+
+import java.io.IOException;
+
+public class URI1095 {
+	
+	private int j,i;
+	
+	public static void main(String[] args) throws IOException{
+		URI1095 uri = new URI1095();
+		uri.loop();
+	}
+	
+	private void loop() {
+		int j = 60;
+		int i = 1;
+		
+		while(j > -1) {
+			System.out.printf("I=%d J=%d\n",i,j);
+			j -= 5;
+			i += 3;
+		}
+		
+		System.out.println("Vai se foder");
+	}
+}
