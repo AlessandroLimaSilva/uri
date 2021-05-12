f49829da2911c7cd07a5c0d4f33de55d6c1e01b4
AlessandroLimaSilva <81444457+AlessandroLimaSilva@users.noreply.github.com>
2021-05-12 00:04:08 -0300

    Update URI1094.java

--- src/desafio/desafio/URI1094.java
+++ src/desafio/desafio/URI1094.java
@@ -1,52 +1,64 @@
 package desafio;
 
+import java.util.Locale;
 import java.util.Scanner;
 import java.io.IOException;
+
 
 public class URI1094 {
 	
 	public static void main(String[] args) throws IOException {
+		Locale.setDefault(Locale.US);
 		URI1094  uri = new URI1094();
 		uri.imprimir();
 	}
 	
 	public void imprimir() {
 		Scanner sc = new Scanner(System.in);
+		
 		int loop = sc.nextInt();
+		
 		int coelho = 0,rato = 0,sapo = 0;
+		
 		for(int i =0;i < loop;i++) {
+			
 			int in = sc.nextInt();
-			String letra = sc.next();
-			if(letra.equalsIgnoreCase("C")) {
-				coelho = in;
+			
+			char letra = sc.next().charAt(0);
+			
+			if(letra == 'C') {
+				coelho += in;
 			}
-			if(letra.equalsIgnoreCase("R")) {
-				rato = in;
+			
+			if(letra == 'R') {
+				rato += in;
 			}
-			if(letra.equalsIgnoreCase("S")) {
-				sapo = in;
+			
+			if(letra == 'S') {
+				sapo += in;
 			}
 		}
 		
 		int soma = coelho+rato+sapo;
-		double porRato = rato * soma / 100;
-		double porSapo = sapo * soma / 100;
-		double porCoelho = coelho * soma / 100;
+		double a  = rato;
+		double b = coelho;
+		double c = sapo;
+		double porRato = (a / soma) * 100;
+		double porSapo = (c / soma) * 100;
+		double porCoelho = (b / soma) * 100;
+		
 		sc.close();
-		System.out.printf("%d",soma);
-		System.out.printf("%d",coelho);
-		System.out.printf("%d",rato);
-		System.out.printf("%d",sapo);
-		System.out.printf("%.2f",porCoelho);
-		System.out.printf("%.2f",porRato);
-		System.out.printf("%.2f",porSapo);
-		System.out.printf("Total: %d de cobaias\n"
-				+ "Total de coelhos: %d\n"
-				+ "Total de ratos: %d\n"
-				+ "Total de sapos: %d\n"
-				+ "Percentual de coelhos: %.2f\n"
-				+ "Percentual de ratos: %.2f\n"
-				+ "Percebtual de sapos: %2.f\n", soma,coelho,rato,sapo,porCoelho,porRato,porSapo);
+		System.out.printf("Total: %d de cobaias\n",soma);
+		System.out.printf("Total de coelhos: %d\n",coelho);
+		System.out.printf("Total de ratos: %d\n",rato);
+		System.out.printf("Total de sapos: %d\n",sapo);
+		System.out.printf("Percentual de coelhos: %.2f %%\n",porCoelho);
+		System.out.printf("Percentual de ratos: %.2f %%\n",porRato);
+		System.out.printf("Percentual de sapos: %.2f %%\n",porSapo);
+		
+		
 	}
+
+	
 
 }
